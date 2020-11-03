b4ee3ac6e68382a7757cb5c1070caa6f5a467e33
yuanbo <115705151@qq.com>
2020-11-03 11:17:11 +0800

    finish basic functionality

--- App.tsx
+++ App.tsx
@@ -8,111 +8,69 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
+  TextInput,
   StatusBar,
+  Button,
+  Alert
 } from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+ 
 
 declare const global: {HermesInternal: null | {}};
 
+let number = Math.round(Math.random() * 100)
+let count = 0;
 const App = () => {
+  
+  const [text, setText] = useState('')
+  function doGuess() {
+    count ++ 
+    const numberValue = parseInt(text)
+    if(numberValue === number) {
+      Alert.alert(`Right! total ${count} times used.`)
+      initNumber() // start next round
+    } else if(numberValue < number) {
+      Alert.alert('too small')
+    } else {
+      Alert.alert('too large')
+    }
+  }
+  function initNumber() {
+    number = Math.round(Math.random() * 100)
+    count = 0
+  }
+  
+  useEffect(() => {
+    initNumber()
+    console.warn(number);
+  }, [])
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+        <TextInput style={styles.input} value={text} onChangeText={setText}></TextInput>
+        <Button title="猜" onPress={doGuess}></Button>
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  input: {
+    borderWidth: 2,
+    borderColor: 'black',
+    margin: 30,
+    height: 50,
+    fontSize: 20,
+    paddingLeft: 10,
+  }
 });
 
 export default App;
